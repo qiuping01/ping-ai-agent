0517da57aff909ef6c96359a36d2a7f9c4045209
qiuping01 <2187718660@qq.com>
2025-08-26 23:21:44 +0800

    文档加载器类 LoveAppDocumentLoader

---
+++ src/main/java/com/ping/aiagent/rag/LoveAppDocumentLoader.java
@@ -0,0 +1,49 @@
+package com.ping.aiagent.rag;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.markdown.MarkdownDocumentReader;
+import org.springframework.ai.reader.markdown.config.MarkdownDocumentReaderConfig;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.ResourcePatternResolver;
+import org.springframework.stereotype.Component;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Slf4j
+@Component
+public class LoveAppDocumentLoader {
+
+    // 构造器注入
+    private final ResourcePatternResolver  resourcePatternResolver;
+
+    public LoveAppDocumentLoader(ResourcePatternResolver resourcePatternResolver) {
+        this.resourcePatternResolver = resourcePatternResolver;
+    }
+
+    /**
+     * 加载多篇 Markdown文件
+     * @return
+     */
+    public List<Document> loadMarkdowns(){
+        List<Document> allDocuments = new ArrayList<>();
+        try {
+            Resource[] resources = resourcePatternResolver.getResources("classpath:document/*.md");
+            for (Resource resource : resources) {
+                String fileName = resource.getFilename();
+                MarkdownDocumentReaderConfig config = MarkdownDocumentReaderConfig.builder()
+                        .withHorizontalRuleCreateDocument(true)
+                        .withIncludeCodeBlock(false)
+                        .withIncludeBlockquote(false)
+                        .withAdditionalMetadata("filename", fileName) // 将文件名添加到元数据中
+                        .build();
+                MarkdownDocumentReader markdownDocumentReader = new MarkdownDocumentReader(resource, config);
+                allDocuments.addAll(markdownDocumentReader.get());
+            }
+        } catch (Exception e) {
+            log.error("Markdown 文档加载失败",e);
+        }
+        return allDocuments;
+    }
+}
