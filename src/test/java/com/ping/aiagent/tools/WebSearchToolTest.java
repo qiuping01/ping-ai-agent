835eae4c9b4a9244cd0cefb0779aa646c8536d37
qiuping01 <2187718660@qq.com>
2025-09-11 00:18:10 +0800

    工具调用：联网搜索 使用searchAPI调用百度进行搜索

---
+++ src/test/java/com/ping/aiagent/tools/WebSearchToolTest.java
@@ -0,0 +1,23 @@
+package com.ping.aiagent.tools;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class WebSearchToolTest {
+
+    @Value("${search-api.api-key}")
+    private String searchApiKey;
+
+    @Test
+    void searchWeb() {
+        WebSearchTool webSearchTool = new WebSearchTool(searchApiKey);
+        String query = "九三大阅兵的重要意义是什么";
+        String result = webSearchTool.searchWeb(query);
+        Assertions.assertNotNull(result);
+    }
+}
