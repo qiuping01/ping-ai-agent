0fdca04b229226b809abf9a8b9685534466d841c
qiuping01 <2187718660@qq.com>
2025-09-11 01:00:28 +0800

    工具调用：终端操作

--- src/test/java/com/ping/aiagent/tools/WebScrapingToolTest.java
+++ src/test/java/com/ping/aiagent/tools/WebScrapingToolTest.java
@@ -2,9 +2,11 @@ package com.ping.aiagent.tools;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@SpringBootTest
 class WebScrapingToolTest {
 
     @Test
