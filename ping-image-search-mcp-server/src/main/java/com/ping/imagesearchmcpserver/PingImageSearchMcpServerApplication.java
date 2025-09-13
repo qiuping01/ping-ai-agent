68358dd785612d668c11782fd7bbd3ec6616e6bb
qiuping01 <2187718660@qq.com>
2025-09-13 16:16:17 +0800

    MCP 协议：MCP 服务端开发

---
+++ ping-image-search-mcp-server/src/main/java/com/ping/imagesearchmcpserver/PingImageSearchMcpServerApplication.java
@@ -0,0 +1,24 @@
+package com.ping.imagesearchmcpserver;
+
+import com.ping.imagesearchmcpserver.tools.ImageSearchTool;
+import org.springframework.ai.tool.ToolCallbackProvider;
+import org.springframework.ai.tool.method.MethodToolCallbackProvider;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class PingImageSearchMcpServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PingImageSearchMcpServerApplication.class, args);
+    }
+
+    @Bean
+    public ToolCallbackProvider imageSearchTools(ImageSearchTool imageSearchTool) {
+        return MethodToolCallbackProvider.builder()
+                .toolObjects(imageSearchTool)
+                .build();
+    }
+
+}
---
+++ ping-image-search-mcp-server/src/test/java/com/ping/imagesearchmcpserver/PingImageSearchMcpServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ping.imagesearchmcpserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingImageSearchMcpServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
