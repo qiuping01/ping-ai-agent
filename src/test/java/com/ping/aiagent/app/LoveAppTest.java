eee2af7d6f26663af62fd225ef63477dd58357ed
qiuping01 <2187718660@qq.com>
2025-09-13 16:49:40 +0800

    MCP协议：测试 stdio 传输方式 1.传输原理：客户端在本地子进程执行服务端的jar包 2.需要在json文件中添加服务端执行方式 3.json代码： {   "mcpServers": {     "amap-maps": {       "command": "npx.cmd",       "args": [         "-y",         "@amap/amap-maps-mcp-server"       ],       "env": {         "AMAP_MAPS_API_KEY": "高德APIKEY"       }     },     "ping-image-search-mcp-server": {       "command": "java",       "args": [         "-Dspring.ai.mcp.server.stdio=true",         "-Dspring.main.web-application-type=none",         "-Dlogging.pattern.console=",         "-jar",         "ping-image-search-mcp-server/target/ping-image-search-mcp-server-0.0.1-SNAPSHOT.jar"       ],       "env": {}     }   } }

--- src/test/java/com/ping/aiagent/app/LoveAppTest.java
+++ src/test/java/com/ping/aiagent/app/LoveAppTest.java
@@ -84,8 +84,13 @@ class LoveAppTest {
     @Test
     void doChatWithMcp() {
         String chatId = UUID.randomUUID().toString();
-        String message = "我的另一半居住在上海静安区，请帮我找到5公里内合适的约会地点";
-        String result = loveApp.doChatWithMcp(message, chatId);
-        Assertions.assertNotNull(result);
+        // 测试地图 MCP
+//        String message = "我的另一半居住在上海静安区，请帮我找到5公里内合适的约会地点";
+//        String result = loveApp.doChatWithMcp(message, chatId);
+//        Assertions.assertNotNull(result);
+        // 测试图片搜索 MCP
+        String message = "帮我搜索一些哄另一半开心的图片";
+        String answer = loveApp.doChatWithMcp(message, chatId);
+        Assertions.assertNotNull(answer);
     }
 }
